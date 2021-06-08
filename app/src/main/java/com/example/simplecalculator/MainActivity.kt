ce83e627081fda273e195d073d93c7c21eaf631e
Yash Gupta <yashguptaaa@gmail.com>
2021-06-08 23:34:34 +0530

    Solved issue of multiple expressions

--- app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -9,10 +9,22 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     var isNewOp = true
     var oldnum =""
+    var newnum =""
     var op = "+"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun calculate( x: Double,  y: Double):Double{
+        var result = 0.0
+        when(op){
+            "+"-> {result = x.toDouble() + y.toDouble()}
+            "-"-> {result = x.toDouble() - y.toDouble()}
+            "*"-> {result = x.toDouble() * y.toDouble()}
+            "/"-> {result = x.toDouble() / y.toDouble()}
+            "%"-> {result = (x.toDouble()/100)* y.toDouble()}
+        }
+        return result
     }
     fun noevent(view: View) {
         if (isNewOp)
@@ -39,7 +51,11 @@ class MainActivity : AppCompatActivity() {
 
     fun opevent(view: View) {
         isNewOp=true
-        oldnum=entry.text.toString()
+        if (oldnum==""){
+        oldnum=entry.text.toString()}
+        else{
+            newnum =entry.text.toString()
+        }
         var buselect = view as Button
         when(buselect.id){
             into.id -> {op ="*"}
@@ -49,25 +65,26 @@ class MainActivity : AppCompatActivity() {
             percentage.id ->{op="%"}
         }
         entry.setText(op)
+        if(newnum!=""){
+            oldnum=(calculate(oldnum.toDouble(),newnum.toDouble())).toString()
+        }
     }
+
 
     fun equalevent(view: View) {
         isNewOp= true
-        var newnum = entry.text.toString()
-        var result = 0.0
-        when(op){
-            "+"-> {result = oldnum.toDouble() + newnum.toDouble()}
-            "-"-> {result = oldnum.toDouble() - newnum.toDouble()}
-            "*"-> {result = oldnum.toDouble() * newnum.toDouble()}
-            "/"-> {result = oldnum.toDouble() / newnum.toDouble()}
-            "%"-> {result = (oldnum.toDouble()/100)* newnum.toDouble()}
-        }
-        entry.setText(result.toString())
+        newnum = entry.text.toString()
+        oldnum=(calculate(oldnum.toDouble(),newnum.toDouble())).toString()
+        entry.setText(oldnum)
+        newnum=""
+        oldnum=""
     }
 
     fun clearAll(view: View) {
         entry.setText("0")
         isNewOp=true
+        oldnum=""
+        newnum=""
     }
 
     fun backspace(view: View) {
@@ -78,6 +95,8 @@ class MainActivity : AppCompatActivity() {
         val temp=entry.text.toString()
         if (temp.isEmpty()){entry.setText("0")
             isNewOp=true
+            oldnum=""
+            newnum=""
         }
     }
 }
